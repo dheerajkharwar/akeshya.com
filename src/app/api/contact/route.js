b6ba8a96ec5776ed00de3f99439a500301bbc916
dheerajkharwar <dheerajkharwar9@gmail.com>
2024-06-22 14:43:38 +0530

    send mail coded

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,13 +1,39 @@
 import { NextResponse } from "next/server";
+const nodemailer = require("nodemailer");
 
 export const runtime = "edge";
 
 export async function POST(req) {
   const res = await req.json();
-  console.log(res.name);
-  console.log(res.email);
-  console.log(res.subject);
-  console.log(res.body);
+  let name = res.name;
+  let email = res.email;
+  let subject = res.subject;
+  let message = res.message;
 
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  } catch (error) {
+    console.log("err1:", error);
+  }
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.SMTP_EMAIL, // sender address
+      to: process.env.TO_EMAIL, // list of receivers
+      subject: subject, // Subject line
+      text: message, // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+  } catch (error) {
+    console.log("err2:", error);
+  }
+  console.log("Message sent: %s", info.messageId);
   return NextResponse.json("Success");
 }
