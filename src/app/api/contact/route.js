4a809ff69437c6e61e658dd6f16c8778ed6473ca
dheerajkharwar <dheerajkharwar9@gmail.com>
2024-06-22 15:29:17 +0530

    nodemailer removed

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-const nodemailer = require("nodemailer");
+// import { createTransport } from "nodemailer";
 
 export const runtime = "edge";
 
@@ -10,30 +10,32 @@ export async function POST(req) {
   let subject = res.subject;
   let message = res.message;
 
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: false, // Use `true` for port 465, `false` for all other ports
-      auth: {
-        user: process.env.SMTP_EMAIL,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
-  } catch (error) {
-    console.log("err1:", error);
-  }
-  try {
-    const info = await transporter.sendMail({
-      from: process.env.SMTP_EMAIL, // sender address
-      to: process.env.TO_EMAIL, // list of receivers
-      subject: subject, // Subject line
-      text: message, // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-  } catch (error) {
-    console.log("err2:", error);
-  }
-  console.log("Message sent: %s", info.messageId);
+  console.log(name, email, subject, message);
+
+  // try {
+  //   const transporter = createTransport({
+  //     host: process.env.SMTP_HOST,
+  //     port: process.env.SMTP_PORT,
+  //     secure: false, // Use `true` for port 465, `false` for all other ports
+  //     auth: {
+  //       user: process.env.SMTP_EMAIL,
+  //       pass: process.env.SMTP_PASSWORD,
+  //     },
+  //   });
+  // } catch (error) {
+  //   console.log("err1:", error);
+  // }
+  // try {
+  //   const info = await transporter.sendMail({
+  //     from: process.env.SMTP_EMAIL, // sender address
+  //     to: process.env.TO_EMAIL, // list of receivers
+  //     subject: subject, // Subject line
+  //     text: message, // plain text body
+  //     html: "<b>Hello world?</b>", // html body
+  //   });
+  // } catch (error) {
+  //   console.log("err2:", error);
+  // }
+  // console.log("Message sent: %s", info.messageId);
   return NextResponse.json("Success");
 }
